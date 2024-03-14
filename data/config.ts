b4253fb52d6002b3f74a127d62b17d81fcbeed53
Aleksandar Timic <aleksandar.timic@meld.com>
2024-03-14 15:36:30 +0100

    Update

--- data/config.ts
+++ data/config.ts
@@ -4,8 +4,8 @@ export const config = {
   ticker: "$ASS",
   contractAddress: "0x13432412424234234234234234",
   website: "https://www.fatasscoin.com",
-  twitter: "https://twitter.com",
-  telegram: "https://telegram.com",
+  twitter: "https://twitter.com/FatAssCoin",
+  telegram: "https://t.me/fatasscoin",
   dexscreener: "https://dexscreener.com",
   buyLink: 'https://raydium.io/'
 };
